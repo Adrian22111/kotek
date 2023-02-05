9c992e46bdba84eadf51463af5201dc30158dd85
unknown <adrianszymczyk2211@gmail.com>
2023-02-05 18:54:20 +0100

    dodanie ograniczenia jedno polecenia na raz

--- animationScripts.js
+++ animationScripts.js
@@ -1,3 +1,4 @@
+/* wymuszenie odtworzenia animacji*/
 function requestAnimation(stringClassName,element)
 {
     requestAnimationFrame((time) => {
@@ -7,16 +8,31 @@ function requestAnimation(stringClassName,element)
         });
     });
 }
+/* funkcja wyłącza przyciski o określonym id na określony czas*/ 
+function disableButtonsForTime(firstButton,secondButton,thirdButton,time)
+{
+    document.getElementById(firstButton).disabled = true;
+    document.getElementById(secondButton).disabled = true;
+    document.getElementById(thirdButton).disabled = true;
+    setTimeout(() => {
+        document.getElementById(firstButton).disabled = false;
+        document.getElementById(secondButton).disabled = false;
+        document.getElementById(thirdButton).disabled = false;
+        }, time);
+}
 
+/* funckja obsługująca odtwarzanie animacji spaceru */
 function orderWalk()
 {
       /* jeśli kot siedzi i chce wykonać animacje chodzenia to musi najpierw wstać*/
      if( document.querySelector(".cat").className == "cat" || document.querySelector(".cat").className == "cat catSittingDown")
      {
          orderGetUpAndWalk();
+         
      }
      else
      {  
+        disableButtonsForTime("orderWalk","orderRun","orderSitDown",10000);
         if(document.querySelector(".cat").className = "cat catWalking") /* Jeśli kot wcześniej chodził to resetuje grafike kota do pozycji stojącej */ 
         {
             document.querySelector(".cat").className = "cat catStanding";
@@ -29,13 +45,19 @@ function orderWalk()
         requestAnimation("container containerWalk",".container");
         /* dodanie animacji do kota */
         requestAnimation("cat catWalking",".cat")
+        
      }
 
 
 }
 
+/* funckja obsługująca odtwarzanie animacji wstawania */
 function orderGetUp()
 {
+    document.getElementById("orderGetUp").disabled = true;
+    document.getElementById("orderSitDown").disabled = false;
+    // disableButtonsForTime("orderWalk","orderGetUp","orderRun","orderSitDown",500);
+
     /* Kotek może wstać tylko jeśli siedzi, jeżeli chodził albo już wstał to nie może wstać*/
     if(document.querySelector(".cat").className == "cat" || document.querySelector(".cat").className == "cat catSittingDown"  )
     {
@@ -44,55 +66,57 @@ function orderGetUp()
             document.querySelector(".container").className = "container";
         }
         requestAnimation("cat catGettingUp",".cat");
-
-    }
-    else
-    {
         
     }
 
-
-    
 }
+/* funckja obsługująca odtwarzanie animacji siadania */
 function orderSitDown()
 {
-    /* Jeżeli kotek siedzi to nie może usiąść jeszcze raz*/
-    if(document.querySelector(".cat").className == "cat" || document.querySelector(".cat").className == "cat catSittingDown")
-    {
 
-    }
-    else
+    /* Jeżeli kotek siedzi to nie może usiąść jeszcze raz*/
+    
+    if(document.querySelector(".cat").className != "cat" || document.querySelector(".cat").className != "cat catSittingDown")
     {
+        document.getElementById("orderSitDown").disabled = true;
+        document.getElementById("orderGetUp").disabled = false;
         if(document.querySelector(".container").className = "container containerWalk")/* Jeśli kot wcześniej chodzi*/ 
         {
             document.querySelector(".container").className = "container";
         }
         requestAnimation("cat catSittingDown",".cat");
+        
     }
- 
-}
 
+}
+/* funckja obsługująca odtwarzanie animacji wstawania i spaceru  */
 function orderGetUpAndWalk()
 {
-    if(document.querySelector(".container").className = "container containerWalk")/* Jeśli kot wcześniej chodził  to resetuje tło do pozycji wyjściowej*/ 
+    disableButtonsForTime("orderWalk","orderRun","orderSitDown",10500);
+    document.getElementById("orderGetUp").disabled = true;
+    /* Jeśli kot wcześniej chodził  to resetuje tło do pozycji wyjściowej*/ 
+    if(document.querySelector(".container").className = "container containerWalk")
     {
         document.querySelector(".container").className = "container";
     }
     /* "dodanie animacji" do tła, jeśli jest sama klasa container to nie ma ona animacji a dzięki jej resetowaniu powyżej można ją wykonywać wielokrotnie bez konieczności odświerzania */
     requestAnimation("container containerGetUpAndWalk",".container");
     requestAnimation("cat catGettingUpAndWalking",".cat");
+    
 }
 
-
+/* funckja obsługująca odtwarzanie animacji biegu */
 function orderRun()
 {
     /* Jeżeli kot siedzi to tak jak w przypadku chodzenia musi najpierw wstać żeby biec*/
+    
     if(document.querySelector(".cat").className == "cat" || document.querySelector(".cat").className == "cat catSittingDown")
     {
         orderGetUpAndRun();
     }
     else
     {
+        disableButtonsForTime("orderWalk","orderRun","orderSitDown",5000);
         if(document.querySelector(".container").className = "container containerRun")
         {
             document.querySelector(".container").className = "container";
@@ -103,13 +127,15 @@ function orderRun()
             document.querySelector(".cat").className = "cat catStanding" 
         }
         requestAnimation("cat catRunning",".cat");
-    
+        
     }
 }
 
-
+/* funckja obsługująca odtwarzanie animacji wstawania i biegu */
 function orderGetUpAndRun()
 {
+    disableButtonsForTime("orderWalk","orderRun","orderSitDown",5500); 
+    document.getElementById("orderGetUp").disabled = true;
     if(document.querySelector(".container").className = "container containerRun")/* Jeśli kot wcześniej chodził  to resetuje tło do pozycji wyjściowej*/ 
     {
         document.querySelector(".container").className = "container";
@@ -117,4 +143,5 @@ function orderGetUpAndRun()
     /* "dodanie animacji" do tła, jeśli jest sama klasa container to nie ma ona animacji a dzięki jej resetowaniu powyżej można ją wykonywać wielokrotnie bez konieczności odświerzania */
     requestAnimation("container containerGetUpAndRun",".container");
     requestAnimation("cat catGettingUpAndRunning",".cat");
+    
 }
